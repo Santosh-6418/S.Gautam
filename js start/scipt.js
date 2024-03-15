0c1f8a891e6d13c7e770febd6d68c640d1d5e770
Santosh Gautam <santosgautam240@gmail.com>
2024-03-15 10:44:17 +0545

    first commit

---
+++ js start/scipt.js
@@ -0,0 +1,29 @@
+/*let a="hello";
+let isAbsent ="true"
+console.log (typeof a);*/
+/*console.log(c);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+if?(a>b){
+    console.log("a is greater t")
+} else{
+    console.log("b is greater")
+}*/
+//create a simple calculator using operator and if esle statement
+//let username =prompt("Enter your name");
+//console.log(username)
+let a=prompt("Enter first number")
+let op =prompt("Enter operator.(eg.+,-,*,/,%")");
+let b= Number(prompt("Enter second number"));
+let (op=="+");0
+console.log (a+b);{
+}
+else if (op =="-"){
+    console.log(a-b);
+} else{
+    console.log("invalid operation");
+}
